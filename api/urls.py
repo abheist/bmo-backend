9e783c032bde5947207246b62119069f1ea9ede2
Abhishek <abhishek291093@gmail.com>
2020-01-07 17:33:34 +0530

    AKS | users detail | created view and used user serializer

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@ from allauth.account.views import confirm_email
 from django.conf.urls import url
 from django.urls import path, include
 
-from api.views import UsersList
+from api.views import UserList, UserDetail
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^verify-email/(?P<key>[\w@+-:]+)/$', confirm_email, name="account_confirm_email"),
-    url(r'^users/$', UsersList.as_view(), name="users_list"),
+    url(r'^user/$', UserList.as_view(), name="users_list"),
+    url(r'^user/(?P<pk>[\w])/$', UserDetail.as_view(), name="user_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from accounts.models import User
 from api.serializers.UserSerializer import UserSerializer
 
 
-class UsersList(ListCreateAPIView):
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
