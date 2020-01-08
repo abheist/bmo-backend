d1e960f59b227fddaf5917d48739f7fa74d42447
Abhishek <abhishek291093@gmail.com>
2020-01-08 17:58:40 +0530

    AKS | users | added user role | [user, admin, superuser] | added migration file to the git

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    USER_TYPE_CHOICES = (
+        (1, 'user'),
+        (2, 'admin'),
+        (3, 'superuser'),
+    )
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=USER_TYPE_CHOICES[0][0])
